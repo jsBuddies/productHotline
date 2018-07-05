c5e6c20d56fff16102a846f36c98f59af2404d04
Jackie Lealess <jlealess@gmail.com>
2018-07-05 14:57:05 -0400

    Adding styles for components at large size

--- src/components/ProductGrid/ProductGrid.js
+++ src/components/ProductGrid/ProductGrid.js
@@ -7,21 +7,34 @@ const ProductGrid = (props) => {
             <h2 className="product-grid__headline">Products</h2>
             <ul className="product-grid">
                 {props.products !== null ? Object.keys(props.products).sort().reverse().map((key) => {
-                    return (
-                        <li className="product" key={key} index={key}>
-                            <a href={`/products/${key}`} className="product__image__link">
-                                <img className="product__image" src={props.products[key].imageLink} alt={props.products[key].name} />
-                            </a>
-                            <h3 className="product__headline">
-                                <a className="product__name__link" href={`/products/${key}`}>
-                                    <span className="product__name__brand">{props.products[key].brand}</span> <span className="product__name">{props.products[key].name}</span>
-                                </a>
-                            </h3>
-                            <p className="product__price">${props.products[key].price}</p>
-                            {props.currentUserRole === 'admin' && <button onClick={() => props.removeItem(key)}>Remove</button>}
-                            {props.currentUserRole === 'admin' && <button onClick={() => props.editItem(key)}>Edit</button>}
-                        </li>
-                    )
+                    return <li className="product" key={key} index={key}>
+                        <a href={`/products/${key}`} className="product__image__link">
+                          <img className="product__image" src={props.products[key].imageLink} alt={props.products[key].name} />
+                        </a>
+                        <h3 className="product__headline">
+                          <a className="product__name__link" href={`/products/${key}`}>
+                            <span className="product__name__brand">
+                              {props.products[key].brand}
+                            </span> <span className="product__name">
+                              {props.products[key].name}
+                            </span>
+                          </a>
+                        </h3>
+                        <p className="product__price">
+                          <span class="product__price__currency">
+                            $
+                          </span>
+                          {props.products[key].price}
+                        </p>
+                        {props.currentUserRole === "admin" && <div className="product__buttons">
+                            <button className="product__button" onClick={() => props.removeItem(key)}>
+                              <i class="far fa-times-circle" /> Remove
+                            </button>
+                            <button className="product__button" onClick={() => props.editItem(key)}>
+                              <i class="far fa-edit" /> Edit
+                            </button>
+                          </div>}
+                      </li>;
                 }) : null}
             </ul>
         </div>
